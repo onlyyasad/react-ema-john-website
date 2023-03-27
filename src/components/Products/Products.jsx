0cac79325be03c8881df476a1dbc312092b7ddfc
onlyyasad <onlyyasad@gmail.com>
2023-03-27 21:13:11 +0600

    cart calculation done

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 
 const Products = (props) => {
-
+    const { img, name, price, seller, ratings, id, shipping } = props.data;
+    
+    const sendToCart = () =>{
+        props.setToCart({id, img, name, price, shipping})
+    }
     console.log(props.data)
-    const { img, name, price, seller, ratings } = props.data;
+    
     return (
         <>
-            <div className="card p-2 w-full bg-base-100 border">
+            <div className="card p-0 w-full bg-base-100 border">
                 <figure className="px-2 pt-2">
-                    <img src={img} alt="Shoes" className="rounded-xl" />
+                    <img src={img? img : "#"} alt="Shoes" className="rounded-xl" />
                 </figure>
-                <div className="card-body items-center text-center">
+                <div className="card-body p-3 items-center">
                     <h2 className="card-title">{name}</h2>
-                    <p>Price: ${price}</p>
-                    <p>Menufacturer: {seller}</p>
-                    <p>Rating: {ratings} Stars</p>
-                    <div className="card-actions">
-                        <button className="btn btn-primary">Add to Cart</button>
+                    <p className='font-bold'>Price: ${price}</p>
+                    <div className='text-sm'>
+                        <p>Manufacturer: {seller}</p>
+                        <p>Rating: {ratings} Stars</p>
                     </div>
+                </div>
+                <div className="card-actions justify-center">
+                    <button onClick={() => sendToCart()} className="bg-orange-300 w-full py-2 hover:bg-orange-400 rounded-b-lg font-bold">Add to Cart</button>
                 </div>
             </div>
         </>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,26 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import Products from '../Products/Products';
 
+
 const Shop = () => {
     const [products, setProducts] = useState([]);
 
-    useEffect(() =>{
+    const [cartProducts, setCartProducts] = useState([]);
+
+    const setToCart = (cartData) => {
+        let newCartProducts = [...cartProducts, cartData];
+        setCartProducts(newCartProducts)
+    }
+
+    const totalPrice = cartProducts.reduce((a, b) => a + b.price, 0);
+    const shippingCharge = cartProducts.reduce((a, b) => a + b.shipping, 0);
+    const tax = parseFloat(((totalPrice + shippingCharge) * 0.1).toFixed(2));
+    const grandTotal = totalPrice + shippingCharge + tax;
+    
+    useEffect(() => {
         fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data))
+            .then(res => res.json())
+            .then(data => setProducts(data))
     }, [])
 
     return (
-        <div className='grid grid-cols-12 mt-4'>
+        <div className='grid grid-cols-12 mt-4 relative'>
             <div className='col-span-9'>
-                <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
+                <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 p-4'>
                     {
-                        products.map((product) => <Products data={product} key={product.id}></Products>)
+                        products.map((product) => <Products
+                            data={product}
+                            key={product.id}
+                            setToCart = {setToCart}
+                        >
+                        </Products>)
                     }
                 </div>
             </div>
-            <div className='col-span-3'>
-                This is Cart Area
+            <div className='col-span-3 p-4 sticky top-0 h-screen'>
+                <div>
+                    <h1 className='text-lg font-bold text-center'>Order Summery</h1>
+                </div>
+                <div className='text-start flex flex-col gap-4 my-8'>
+                    <p>Selected Items: <span>{cartProducts.length}</span></p>
+                    <p>Total Price: $<span id='total-price'>{totalPrice}</span></p>
+                    <p>Total Shipping Charge: $<span id='shipping-charge'>{shippingCharge}</span></p>
+                    <p>Tax: $ <span id='tax'>{tax}</span> </p>
+                    <p className='text-lg font-semibold'>Grand Total: {grandTotal}</p>
+                </div>
+                <div className='flex flex-col gap-4'>
+                    <button className='bg-red-500 text-white font-semibold p-2 rounded-md'>Clear Cart</button>
+                    <button className='bg-orange-500 text-white font-semibold p-2 rounded-md'>Review Order</button>
+                </div>
             </div>
         </div>
     );
